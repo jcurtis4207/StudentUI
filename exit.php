b2efb33ad9eb954c123d658b3541a15f377bfb95
Jacob Curtis <jcurtis4207@gmail.com>
2020-10-21 16:14:46 -0400

    restructure folders, remove old tests

---
+++ exit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>RateMyLab</title>
+</head>
+<body>
+    <?php
+        session_start();
+        echo "All " . $_SESSION['total_questions'] . " Questions Rated";
+        echo "<br>Thank you for rating the lab";
+        session_unset();
+        session_destroy();
+    ?>
+</body>
+</html>
